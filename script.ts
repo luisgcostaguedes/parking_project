f2dbe6aff89f536ccb82ef6a0eb0cd1c34337eb1
luisgcostaguedes <luisgcostaguedes@gmail.com>
2024-01-05 10:35:17 -0300

    up script

--- script.ts
+++ script.ts
@@ -1,3 +1,23 @@
 (function () {
-  const $ = (query: string) => document.querySelector(query);
+  const $ = (query: string): HTMLInputElement | null =>
+    document.querySelector(query);
+  function parking() {
+    function read() {}
+    function create() {}
+    function remove() {}
+    function save() {}
+    function render() {}
+
+    return { read, create, remove, save, render };
+  }
+
+  $("#cadastrar")?.addEventListener("click", () => {
+    const marca = $("#marcaCarro")?.value;
+    const placa = $("#placaCarro")?.value;
+
+    if (!marca || !placa) {
+      alert("Preencha os campos obrigatórios");
+      return;
+    }
+  });
 })();
